4448f0e6f0263da04b8987ed0d8c46b7bc562672
Matt Wong <matt.wong.rpi@gmail.com>
2020-05-13 23:30:25 -0400

    refactor: ♻️ remove list-items component class

---
+++ src/task-list/loading-row.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export const LoadingRow: React.FC = () => {
+  return (
+    <div className="loading-item flex h-12 w-full bg-white items-center leading-4 pl-4">
+      <span className="glow-item mr-4 w-3 h-3" />
+      <span>
+        <span className="glow-item">Loading</span>&nbsp;<span className="glow-item">cool</span>&nbsp;
+        <span className="glow-item">state</span>
+      </span>
+    </div>
+  );
+};
--- src/task-list/task-list.tsx
+++ src/task-list/task-list.tsx
@@ -3,6 +3,7 @@ import Task, { TaskInfo } from '../task';
 import Checkmark from '../icons/Checkmark';
 import Message from '../message';
 import { useTasksContext } from '../tasks-context';
+import { LoadingRow } from './loading-row';
 
 export interface TaskListProps {
   loading?: boolean;
@@ -14,50 +15,33 @@ export interface TaskListProps {
 export const TaskList: React.FC<TaskListProps> = (props) => {
   const { loading = false, tasks, onPinTask, onArchiveTask } = props;
   const events = { onPinTask, onArchiveTask };
-
-  const LoadingRow = (
-    <div className="loading-item flex h-12 w-full bg-white items-center leading-4 pl-4">
-      <span className="glow-item mr-4 w-3 h-3" />
-      <span>
-        <span className="glow-item">Loading</span>&nbsp;<span className="glow-item">cool</span>&nbsp;
-        <span className="glow-item">state</span>
-      </span>
-    </div>
-  );
+  let content;
 
   if (loading) {
-    return (
-      <div className="list-items">
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-      </div>
+    content = (
+      <>
+        <LoadingRow />
+        <LoadingRow />
+        <LoadingRow />
+        <LoadingRow />
+        <LoadingRow />
+        <LoadingRow />
+      </>
     );
+  } else if (tasks.length === 0) {
+    content = <Message icon={Checkmark} title="You have no tasks." subtitle="Sit back and relax." />;
+  } else {
+    const sortedTasks: TaskInfo[] = [
+      ...tasks.filter((t) => t.state === 'TASK_PINNED'),
+      ...tasks.filter((t) => t.state !== 'TASK_PINNED'),
+    ];
+
+    content = sortedTasks.map((task) => {
+      return <Task key={task.id} {...task} {...events} />;
+    });
   }
 
-  if (tasks.length === 0) {
-    return (
-      <div className="list-items">
-        <Message icon={Checkmark} title="You have no tasks." subtitle="Sit back and relax." />
-      </div>
-    );
-  }
-
-  const sortedTasks: TaskInfo[] = [
-    ...tasks.filter((t) => t.state === 'TASK_PINNED'),
-    ...tasks.filter((t) => t.state !== 'TASK_PINNED'),
-  ];
-
-  return (
-    <div className="list-items">
-      {sortedTasks.map((task) => {
-        return <Task key={task.id} {...task} {...events} />;
-      })}
-    </div>
-  );
+  return <div className="relative bg-white min-h-72">{content}</div>;
 };
 
 const TaskListContainer: React.FC = () => {
